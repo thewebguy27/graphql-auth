c0e5472cd8610a8320b98b6aee3ad2020d536534
thewebguy27 <hamzaawan2277@gmail.com>
2024-03-18 16:20:10 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import createApolloGraphqlServer from "./graphql";
+import UserService from "./services/user";
+
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
+
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
+
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        const token = req.headers["token"];
+
+        try {
+          const user = UserService.decodeJWTToken(token as string);
+          return { user };
+        } catch (error) {
+          return {};
+        }
+      },
+    })
+  );
+
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
+}
+
+init();
